3d4aa8eea596de908396913b26e9ae1871ce8bc4
kodai731 <kodai731@gmail.com>
2025-11-09 13:42:17 +0900

    work test addon

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn blender_addon_test(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn blender_addon_core(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     Ok(())
 }
